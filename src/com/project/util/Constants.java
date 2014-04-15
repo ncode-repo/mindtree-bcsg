3d3a5a705cf752f6e7e4a556e7ada83b612811c5
jaishah <jai_shah@mindtree.com>
2014-04-15 11:26:45 +0530

    Created a base webapp

--- src/com/project/util/Constants.java
+++ src/com/project/util/Constants.java
@@ -1,16 +1,5 @@
 package com.project.util;
 
 public class Constants {
-	public static final String BEAN_LOCATION = "C:\\MBW\\mastercard\\src\\com\\bcsg\\saas\\web\\forms\\registration\\SelfServeProvisionForm.java";
-	public static final String TEMPLATE_LOCATION = "/WEB-INF/template/template.jsp";
-	public static final String PROP_LOCATION ="./conf/conf.properties";
-	public static final String BUTTON ="button";
-	public static final String EVENT ="event";
-	public static final String TAG ="{tag}";
-	public static final String TEXT = "text";
-	public static final String TEXT_TAG = "textTag";
-	public static final String BUTTON_TAG ="buttonTag";
-	public static final String KEYWORD_PRIVATE ="private";
-	public static final String KEYWORD_FINAL ="final";
-	
+
 }

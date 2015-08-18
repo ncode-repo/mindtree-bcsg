e7556b89fbdd10cedded72fdeaf3eb2373c0c60c
Nilesh <M1019915@mindtree.com>
2015-08-18 14:43:17 +0530

    Code formatted

--- src/ws/Hp_POC.java
+++ src/ws/Hp_POC.java
@@ -1,4 +1,5 @@
 package ws;
+
 import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
 
@@ -24,29 +25,23 @@ public class Hp_POC {
 		// TODO Auto-generated method stub
 		JSONObject json = new JSONObject(); //
 		// Create a trust manager that does not validate certificate chains
-		TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
-			public X509Certificate[] getAcceptedIssuers() {
-				return null;
-			}
-
-			public void checkClientTrusted(X509Certificate[] certs,
-					String authType) {
-			}
-
-			public void checkServerTrusted(X509Certificate[] certs,
-					String authType) {
-			}
-		} };
-
-		// Install the all-trusting trust manager
-		try {
-			SSLContext sc = SSLContext.getInstance("TLS");
-			sc.init(null, trustAllCerts, new SecureRandom());
-			HttpsURLConnection
-					.setDefaultSSLSocketFactory(sc.getSocketFactory());
-		} catch (Exception e) {
-			;
-		}
+		/*
+		 * TrustManager[] trustAllCerts = new TrustManager[] { new
+		 * X509TrustManager() { public X509Certificate[] getAcceptedIssuers() {
+		 * return null; }
+		 * 
+		 * public void checkClientTrusted(X509Certificate[] certs, String
+		 * authType) { }
+		 * 
+		 * public void checkServerTrusted(X509Certificate[] certs, String
+		 * authType) { } } };
+		 * 
+		 * // Install the all-trusting trust manager try { SSLContext sc =
+		 * SSLContext.getInstance("TLS"); sc.init(null, trustAllCerts, new
+		 * SecureRandom()); HttpsURLConnection
+		 * .setDefaultSSLSocketFactory(sc.getSocketFactory()); } catch
+		 * (Exception e) { ; }
+		 */
 		Client client = Client.create();
 		try {
 			JSONObject jsonCred = new JSONObject();
@@ -61,6 +56,9 @@ public class Hp_POC {
 
 		WebResource webResource = client
 				.resource("https://csa45.pocaas.hpintelco.org:8444/idm-service/v2.0/tokens");
+		// WebResource webResource =
+		// client.resource("https://213.30.160.29:8444/idm-service/v2.0/tokens");
+
 		String authorization = "idmTransportUser" + ":" + "idmTransportUser";
 		authorization = headerAuth(authorization);
 
@@ -74,7 +72,7 @@ public class Hp_POC {
 			JSONObject token = result.getJSONObject("token");
 			String token_id = token.getString("id");
 			System.out.println("Token ID: " + token_id);
-			listSvcOfferings(token_id,"");
+			listSvcOfferings(token_id, "");
 			getOfferingDetails(token_id);
 		} catch (Exception e) {
 			System.out.println("Exception message: " + e.getMessage());
@@ -91,7 +89,7 @@ public class Hp_POC {
 	}
 
 	public static void listSvcOfferings(String token, String filter) {
-		JSONObject json = new JSONObject(); 
+		JSONObject json = new JSONObject();
 		Client client = Client.create();
 		try {
 			json.put("approval", "ALL");
@@ -101,6 +99,8 @@ public class Hp_POC {
 		}
 		WebResource webResource = client
 				.resource("https://csa45.pocaas.hpintelco.org:8444/csa/api/mpp/mpp-offering/filter");
+		// WebResource webResource =
+		// client.resource("https://213.30.160.29:8444/csa/api/mpp/mpp-offering/filter");
 		String authorization = "idmTransportUser" + ":" + "idmTransportUser";
 		authorization = headerAuth(authorization);
 		ClientResponse wsResponse = webResource
@@ -113,14 +113,22 @@ public class Hp_POC {
 			System.out.println("Service offerings: " + result.toString());
 		} catch (Exception e) {
 			System.out.println("Exception message: " + e.getMessage());
-			System.out.println("Response message:" + wsResponse.toString() );
+			System.out.println("Response message:" + wsResponse.toString());
 		}
 	}
-	
-	public static void getOfferingDetails(String token){
+
+	public static void getOfferingDetails(String token) {
 		Client client = Client.create();
-		WebResource webResource = client
-				.resource("https://csa45.pocaas.hpintelco.org:8444/csa/api/mpp/mppoffering/8a83d7a44f1d0f21014f1d37683608a1?catalogId=8a83d7a44f1d0f21014f23000f8816df&category=SIMPLE_SYSTEM");
+		String url = "https://csa45.pocaas.hpintelco.org:8444/csa/api/mpp/mppoffering/8a83d7a44f1d0f21014f1d37683608a1";
+		// String
+		// url="https://213.30.160.29:8444/csa/api/mpp/mppoffering/8a83d7a44f1d0f21014f1d37683608a1";
+		// String
+		// catalogDetails="?catalogId=8a83d7a44f1d0f21014f23000f8816df&category=SIMPLE_SYSTEM";
+		// String
+		// catalogDetails="?catalogId=8a83d1be4e975737014ebb4d44485056&category=SIMPLE_SYSTEM";
+		String catalogDetails = "?catalogId=8a83d1be4e53200a014e5336c0211537&category=SIMPLE_SYSTEM";
+
+		WebResource webResource = client.resource(url + catalogDetails);
 		String authorization = "idmTransportUser" + ":" + "idmTransportUser";
 		authorization = headerAuth(authorization);
 		ClientResponse wsResponse = webResource
